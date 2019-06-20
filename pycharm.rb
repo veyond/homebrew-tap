47dfa79f818ff94dcd142950659c098ede091a8c
yangck <yangck@amtxts.com>
2019-06-20 15:23:40 +0800

    pycharm formula

---
+++ pycharm.rb
@@ -0,0 +1,18 @@
+class Pycharm < Formula
+    desc "Python IDE for Professional Developers, professional edition."
+    homepage "http://www.jetbrains.com/pycharm/"
+    version "2019.1.3"
+    url "https://download.jetbrains.com/python/pycharm-professional-#{version}.tar.gz"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/pycharm.sh" => "pycharm")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "pycharm".
+        EOS
+    end
+end
